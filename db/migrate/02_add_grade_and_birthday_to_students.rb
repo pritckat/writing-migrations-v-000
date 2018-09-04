20fc89ffdb11f668967d7416f31b414fbd2e75c2
Katrina Pritchard <pritckat@gmail.com>
2018-09-04 18:37:58 +0000

    Done.

---
+++ db/migrate/02_add_grade_and_birthday_to_students.rb
@@ -0,0 +1,7 @@
+class AddGradeAndBirthdayToStudents < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :students, :grade, :integer
+    add_column :students, :birthdate, :string
+  end
+end
